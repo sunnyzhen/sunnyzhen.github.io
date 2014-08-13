ae41cc7342ff0c04cfd1ec62f6947a219a1abd53
sunnyzhen <287019674@qq.com>
2014-08-13 10:35:32 +0800

    [!]

--- course/animation_principles/js/js.js
+++ course/animation_principles/js/js.js
@@ -157,11 +157,11 @@ $(document).ready(function(){
 	//菜单触按触发事件
 	$(".menu_wrap").bind("touchstart", function(event){
 		
-		alert("menu_wrap");
-		$(".menu_wrap").addClass("hover");
-		alert("menu_wrap_2");
 		if($(".menu_wrap").hasClass("hover")){
 			$(".menu_wrap").removeClass("hover");
+		}
+		else{
+			$(".menu_wrap").addClass("hover");
 		}
 		
 	});
