ec68dc7edf74a4d8e8f0b6c3e7bed3fc5f598224
sunnyzhen <287019674@qq.com>
2014-09-29 16:56:25 +0800

    [!]

--- demo/recruit/js/js.js
+++ demo/recruit/js/js.js
@@ -508,6 +508,67 @@ $(document).ready(function(){
 		$(this).parents(".email_wrap").removeClass("share_tips_show");
 	});
 	
+	
+	/* 安卓版本兼容 */
+	var brower = {
+		versions:function(){
+			var u = window.navigator.userAgent;
+			var num ;
+			if(u.indexOf('Trident') > -1){
+			//IE
+				return "IE";
+			}else if(u.indexOf('Presto') > -1){
+			//opera
+				return "Opera";
+			}else if(u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1){
+			//firefox
+				return "Firefox";
+			}else if(u.indexOf('AppleWebKit' && u.indexOf('Safari') > -1) > -1){
+			//苹果、谷歌内核
+				if(u.indexOf('Chrome') > -1){
+				//chrome
+					return "Chrome";
+				}else if(u.indexOf('OPR')){
+				//webkit Opera
+					return "Opera_webkit"
+				}else{
+				//Safari
+					return "Safari";
+				}
+			}else if(u.indexOf('Mobile') > -1){
+			//移动端
+				if(!!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)){
+				//ios
+					if(u.indexOf('iPhone') > -1){
+					//iphone
+						return "iPhone"
+					}else if(u.indexOf('iPod') > -1){
+					//ipod
+						return "iPod"
+					}else if(u.indexOf('iPad') > -1){
+					//ipad
+						return "iPad"
+					}
+				}else if(u.indexOf('Android') > -1 || u.indexOf('Linux') > -1){
+				//android
+					num = u.substr(u.indexOf('Android') + 8, 3);
+					return {"type":"Android", "version": num};
+				}else if(u.indexOf('BB10') > -1 ){
+				//黑莓bb10系统
+					return "BB10";
+				}else if(u.indexOf('IEMobile')){
+				//windows phone
+					return "Windows Phone"
+				}
+			}
+		}
+    }
+	
+	var system=brower.versions();
+	if(system.type=="Android"){
+		$("body").addClass("android_version");
+	}
+	
 	/*微信转发图片*/
 	
 	var imgUrl = 'http://sunnyzhen.github.io/demo/recruit/images/forward_icon.jpg';
