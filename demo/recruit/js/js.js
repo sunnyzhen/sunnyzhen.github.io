56582233e1bb08196abf2f89a79c73282d3c8ee3
sunnyzhen <287019674@qq.com>
2014-10-09 16:31:03 +0800

    [!]

--- demo/recruit/js/js.js
+++ demo/recruit/js/js.js
@@ -205,7 +205,7 @@ $(document).ready(function(){
 			sub_page_index=0;
 			
 			//安卓4.4兼容，按钮滑屏隐藏bug
-			$(".android_version .tab_wrap").css("z-index","auto");
+			//$(".android_version .tab_wrap").css("z-index","auto");
 		 },
 		 wipeUp: function() { 
 		 	if(wipe_hook==0){
@@ -233,7 +233,7 @@ $(document).ready(function(){
 				sub_page_index=0;
 				
 				//安卓4.4兼容，按钮滑屏隐藏bug
-				$(".android_version .tab_wrap").css("z-index",3);
+				//$(".android_version .tab_wrap").css("z-index",3);
 			}
 		 },
 		 wipeLeft: function() {
