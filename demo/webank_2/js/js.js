aa42d224b1a31c275186e1c979b1ecd654e9cf4e
sunnyzhen <287019674@qq.com>
2014-12-31 23:39:24 +0800

    [!]

--- demo/webank_2/js/js.js
+++ demo/webank_2/js/js.js
@@ -311,6 +311,18 @@ $(document).ready(function(){
 		});
 	}, false);
 	
+	function WeiXinShareBtn() { 
+		if (typeof WeixinJSBridge == "undefined") { 
+			alert("请先通过微信搜索 wow36kr 添加36氪为好友，通过微信分享文章 "); 
+		} else { 
+			WeixinJSBridge.invoke('shareTimeline', { 
+				"title": "36氪", 
+				"link": "http://www.36kr.com", 
+				"desc": "关注互联网创业", 
+				"img_url": "http://www.36kr.com/assets/images/apple-touch-icon.png" 
+			}); 
+		} 
+	}
 	
 });
 
