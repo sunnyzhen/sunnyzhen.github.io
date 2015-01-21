1c58b7c737dbfe370eebbbd1e90ea991944bc7ce
sunnyzhen <287019674@qq.com>
2015-01-21 18:50:54 +0800

    [!]

--- game/dress_up/js/js.js
+++ game/dress_up/js/js.js
@@ -2,70 +2,9 @@
 
 $(document).ready(function(){
 	
-	/*翻页标识*/
-	var page_index=1;//当前在哪个页面
-	var page_total=8;//总共有多少页面
-	var subpage_index=1;//当前在哪个子页
-	var no_wipe=0;
-	var main_list=$("#tab_list_1");
-	
-	function setMainList(num){
-		var str="";
-		for(var i=1;i<=num;i++){
-			if(i==1){
-				str+="<li class='current'><a href='##'><i>"+i+"</i></a></li>";
-			}
-			else if(i==num){
-				str+="<li class='last'><a href='##'><i>"+i+"</i></a></li>";
-			}
-			else{
-				str+="<li><a href='##'><i>"+i+"</i></a></li>";
-			}
-		}
-		$(main_list).html(str);
-	}
-	
-	setMainList(page_total);
-	
-	function setMainListItem(num){
-		$(main_list).children("li").removeClass("current");
-		$($(main_list).children("li")[num-1]).addClass("current");
-		if(page_index==8){
-			$(main_list).fadeOut();
-		}
-		else{
-			$(main_list).fadeIn();
-		}
-	}
-	
-	$(".share_we").click(function(){
-		$(".share_guide_wrap").addClass("show_guide");
-		$(".penguin_wrap_10_hook").css({
-			"top": "-100px"
-		});
-		no_wipe=1;
-	});
-	
-	$(".share_guide_wrap").click(function(){
-		$(".share_guide_wrap").removeClass("show_guide");
-		$(".penguin_wrap_10_hook").css({
-			"top": "0px"
-		});
-		no_wipe=0;
-	});
-	
-	$(".penguin_wrap_10").click(function(){
-		$(".share_guide_wrap").addClass("show_guide");
-		$(".penguin_wrap_10_hook").css({
-			"top": "-100px"
-		});
-		no_wipe=1;
-	});
-	
-	$(".cover_link_next").click(function(){
-		wipe_down();
-	});
-	
+	var item_sel_index=0;
+	var decoration_list=["glass","blush","bowknot"];
+	var decoration_index=0;
 	
 	//触摸触发事件
 	$("body").touchwipe({
@@ -85,8 +24,6 @@ $(document).ready(function(){
 		min_move_y: 80,
 		preventDefaultEvents: true
 	});
-	
-	/*pc test*/
 	
 	//wipeLeft
 	$(".hook_right").bind("click",function(){
@@ -108,87 +45,102 @@ $(document).ready(function(){
 		wipe_down();
 	});
 	
+	
 	function wipe_right(){
-		if(no_wipe==1){
+		if(item_sel_index==1){
+			
+			if(decoration_index==0){
 				
-		}
-		else{
-			if(page_index>=1&&page_index<page_total&&subpage_index==1){
-				$($(".con_wrap")[page_index-1]).addClass("wrap_hide");
-				$($(".con_wrap")[page_index]).addClass("wrap_show");
-				no_wipe=1;
+			}
+			else if(decoration_index==1){
+				$("."+decoration_list[0]).css({
+					"opacity":0
+				});
 				
-				var timer=setTimeout(function(){
-					$($(".con_wrap")[page_index-1]).removeClass("wrap_show");
-					$($(".con_wrap")[page_index]).removeClass("wrap_prepare");
-					no_wipe=0;
-					
-					clearTimeout(timer);
-					page_index++;
-					if(page_index>page_total){
-						page_index=page_total;
-					}
-					
-					setMainListItem(page_index);
-					
-					//console.log(page_index+" "+subpage_index);
-					
-				},300);
+				$("."+decoration_list[decoration_list.length-1]).css({
+					"opacity":1
+				});
+			}
+			else{
+				$("."+decoration_list[decoration_index-1]).css({
+					"opacity":0
+				});
 				
+				$("."+decoration_list[decoration_index-2]).css({
+					"opacity":1
+				});
+			}
+			
+			if(decoration_index==1){
+				decoration_index=decoration_list.length;
+			}
+			else{
+				decoration_index--;
 			}
 		}
 	}
 	
 	function wipe_left(){
-		if(no_wipe==1){
+		if(item_sel_index==1){
+			
+			if(decoration_index==0){
+				$("."+decoration_list[0]).css({
+					"opacity":1
+				});
+			}
+			else if(decoration_index==decoration_list.length){
+				$("."+decoration_list[decoration_list.length-1]).css({
+					"opacity":0
+				});
 				
-		}
-		else{
-			if(page_index>=2&&page_index<=page_total&&subpage_index==1){
-				$($(".con_wrap")[page_index-2]).removeClass("wrap_hide").addClass("wrap_show");
-				$($(".con_wrap")[page_index-1]).removeClass("wrap_show").addClass("wrap_prepare");
+				$("."+decoration_list[0]).css({
+					"opacity":1
+				});
+			}
+			else{
+				$("."+decoration_list[decoration_index-1]).css({
+					"opacity":0
+				});
 				
-				page_index--;
-				
+				$("."+decoration_list[decoration_index]).css({
+					"opacity":1
+				});
 			}
 			
-			setMainListItem(page_index);
+			if(decoration_index==decoration_list.length){
+				decoration_index=1;
+			}
+			else{
+				decoration_index++;
+			}
+			
 		}
 	}
 	
 	function wipe_up(){
-		if(no_wipe==1){
-				
-		}
-		else{
-			if(page_index==page_total){
-				var list_num=$(".details_list .details_con").length;
-				if(subpage_index>=2&&subpage_index<=list_num){
-					$($(".details_con")[subpage_index-2]).removeClass("wrap_before").addClass("current");
-					$($(".details_con")[subpage_index-1]).removeClass("current").addClass("wrap_after");
-					
-					subpage_index--;
-				}
-			}
-		}
+		
 	}
 	
 	function wipe_down(){
-		if(no_wipe==1){
-				
-		}
-		else{
-			if(page_index==page_total){
-				var list_num=$(".details_list .details_con").length;
-				if(subpage_index>=1&&subpage_index<list_num){
-					$($(".details_con")[subpage_index-1]).addClass("wrap_before").removeClass("current");
-					$($(".details_con")[subpage_index]).addClass("current").removeClass("wrap_after");
-					
-					subpage_index++;
-				}
-			}
+		
+	}
+	
+	function BindItemEvent(){
+		var itemlist=$(".operate_list .item");
+		for(var i=1;i<=itemlist.length;i++){
+			(function(){
+				var index=i;
+				$(".operate_list .item_"+index).click(function(){
+					$(".operate_list .item").removeClass("current");
+					$(this).addClass("current");
+					item_sel_index=index;
+				});
+			})();
 		}
 	}
+	
+	BindItemEvent();
+	
 	
 	/*pc test*/
 	
@@ -257,128 +209,6 @@ $(document).ready(function(){
 		}
 	}
 	
-	/*微信转发图片*/
-	
-	var location_str=location.href;
-	var location_reg="mobile/";
-	//stri.split(reg)[0]+"/images/cover/icon_webank.jpg";
-	//console.log(stri.split(reg));
-	//console.log(location.href);
-	//console.log(location.href.split("mobile/")[0]+"mobile/"+"images/cover/icon_webank.jpg");
-	
-	
-	var imgUrl = location_str.split(location_reg)[0]+location_reg+"images/cover/icon_webank.jpg";
-	var lineLink = location.href;
-	var descContent = "我们是银行？我们是互联网？We是互联网银行。";
-	var shareTitle = document.title;
-	var appid = '';
-	
-	function shareFriend() {
-		WeixinJSBridge.invoke('sendAppMessage',{
-			"appid": appid,
-			"img_url": imgUrl,
-			"img_width": "200",
-			"img_height": "200",
-			"link": lineLink,
-			"desc": descContent,
-			"title": shareTitle
-		}, function(res) {
-			//_report('send_msg', res.err_msg);
-		})
-	}
-	function shareTimeline() {
-		WeixinJSBridge.invoke('shareTimeline',{
-			"img_url": imgUrl,
-			"img_width": "200",
-			"img_height": "200",
-			"link": lineLink,
-			"desc": descContent,
-			"title": shareTitle
-		}, function(res) {
-			   //_report('timeline', res.err_msg);
-		});
-	}
-	function shareWeibo() {
-		WeixinJSBridge.invoke('shareWeibo',{
-			"content": descContent,
-			"url": lineLink,
-		}, function(res) {
-			//_report('weibo', res.err_msg);
-		});
-	}
-	// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
-	document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-		// 发送给好友
-		WeixinJSBridge.on('menu:share:appmessage', function(argv){
-			shareFriend();
-		});
-		// 分享到朋友圈
-		WeixinJSBridge.on('menu:share:timeline', function(argv){
-			shareTimeline();
-		});
-		// 分享到微博
-		WeixinJSBridge.on('menu:share:weibo', function(argv){
-			shareWeibo();
-		});
-	}, false);
-	
-	
-	//新版分享JSsdk
-	/*wx.ready(function(){
-		
-		 wx.config({
-			jsApiList: [
-				'checkJsApi',
-				'onMenuShareTimeline',
-				'onMenuShareAppMessage',
-				'onMenuShareQQ',
-				'onMenuShareWeibo'
-			],
-			success: function (res) {
-				alert(JSON.stringify(res));
-			} 
-		}); 
-		
-		 wx.checkJsApi({
-			jsApiList: [
-				'getNetworkType',
-				'previewImage'
-			],
-			success: function (res) {
-			alert(JSON.stringify(res));
-			}
-		});
-		
-		wx.onMenuShareTimeline({
-			'title': shareTitle, // 分享标题
-			'link': lineLink, // 分享链接
-			'imgUrl': imgUrl // 分享图标
-		});
-	
-		wx.onMenuShareAppMessage({
-			'title': shareTitle, // 分享标题
-			'desc': descContent, // 分享描述
-			'link': lineLink, // 分享链接
-			'imgUrl': imgUrl, // 分享图标
-		});
-		
-		wx.onMenuShareQQ({
-			'title': shareTitle, // 分享标题
-			'desc': descContent, // 分享描述
-			'link': lineLink, // 分享链接
-			'imgUrl': imgUrl // 分享图标
-		});
-	
-		wx.onMenuShareWeibo({
-			'title': shareTitle, // 分享标题
-			'desc': descContent, // 分享描述
-			'link': lineLink, // 分享链接
-			'imgUrl': imgUrl // 分享图标
-		});
-	});
-*/
-	
-
 	
 });
 
