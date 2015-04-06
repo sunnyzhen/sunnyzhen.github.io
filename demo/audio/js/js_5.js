a7651249f6050fcaf57051691d04d50d57c4d751
sunnyzhen <287019674@qq.com>
2015-04-06 16:05:37 +0800

    [!]

---
+++ demo/audio/js/js_5.js
@@ -0,0 +1,89 @@
+(function($){$.fn.touchwipe=function(settings){var config={min_move_x:20,min_move_y:20,wipeLeft:function(){},wipeRight:function(){},wipeUp:function(){},wipeDown:function(){},preventDefaultEvents:true};if(settings)$.extend(config,settings);this.each(function(){var startX;var startY;var isMoving=false;function cancelTouch(){this.removeEventListener('touchmove',onTouchMove);startX=null;isMoving=false}function onTouchMove(e){if(config.preventDefaultEvents){e.preventDefault()}if(isMoving){var x=e.touches[0].pageX;var y=e.touches[0].pageY;var dx=startX-x;var dy=startY-y;if(Math.abs(dx)>=config.min_move_x){cancelTouch();if(dx>0){config.wipeLeft(e)}else{config.wipeRight(e)}}else if(Math.abs(dy)>=config.min_move_y){cancelTouch();if(dy>0){config.wipeDown(e)}else{config.wipeUp(e)}}}}function onTouchStart(e){if(e.touches.length==1){startX=e.touches[0].pageX;startY=e.touches[0].pageY;isMoving=true;this.addEventListener('touchmove',onTouchMove,false)}}if('ontouchstart'in document.documentElement){this.addEventListener('touchstart',onTouchStart,false)}});return this}})(jQuery);
+
+$(document).ready(function(){
+	
+	//触摸触发事件
+	$("body").touchwipe({
+		wipeDown: function() {
+			wipe_down();
+		 },
+		 wipeUp: function() { 
+			wipe_up();
+		 },
+		 wipeLeft: function() {
+			wipe_right();
+		 },
+		 wipeRight: function() { 
+		 	wipe_left();
+		 },
+		min_move_x: 80,
+		min_move_y: 80,
+		preventDefaultEvents: true
+	});
+	
+	
+	function wipe_right(){
+		
+	}
+	
+	function wipe_left(){
+		
+	}
+	
+	function wipe_up(){
+		
+	}
+	
+	function wipe_down(){
+		
+	}
+	var audio_2 = "";
+	function musicPlay(){
+		var audio = document.createElement("audio");
+		audio.src = "media/happy.mp3";
+		audio.loop = true;
+		audio.load();
+		audio.addEventListener("canplaythrough", function(){
+			
+			audio.play();
+			
+		},false);
+		
+		$(".audio_wrap").append(audio);
+		
+		audio_2 = document.createElement("audio");
+		audio_2.src = "media/beep.mp3";
+		$(".audio_wrap").append(audio_2);
+		
+	}
+	
+	musicPlay();
+	
+	
+	
+	$(".btn_beep").click(function(){
+		audio_2.play();
+	});
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
