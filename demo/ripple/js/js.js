e4a6c3656ca6c962cf8b9c3666f3949353deaea8
sunnyzhen <287019674@qq.com>
2015-04-09 16:32:12 +0800

    [!]

---
+++ demo/ripple/js/js.js
@@ -0,0 +1,275 @@
+(function($){$.fn.touchwipe=function(settings){var config={min_move_x:20,min_move_y:20,wipeLeft:function(){},wipeRight:function(){},wipeUp:function(){},wipeDown:function(){},preventDefaultEvents:true};if(settings)$.extend(config,settings);this.each(function(){var startX;var startY;var isMoving=false;function cancelTouch(){this.removeEventListener('touchmove',onTouchMove);startX=null;isMoving=false}function onTouchMove(e){if(config.preventDefaultEvents){e.preventDefault()}if(isMoving){var x=e.touches[0].pageX;var y=e.touches[0].pageY;var dx=startX-x;var dy=startY-y;if(Math.abs(dx)>=config.min_move_x){cancelTouch();if(dx>0){config.wipeLeft(e)}else{config.wipeRight(e)}}else if(Math.abs(dy)>=config.min_move_y){cancelTouch();if(dy>0){config.wipeDown(e)}else{config.wipeUp(e)}}}}function onTouchStart(e){if(e.touches.length==1){startX=e.touches[0].pageX;startY=e.touches[0].pageY;isMoving=true;this.addEventListener('touchmove',onTouchMove,false)}}if('ontouchstart'in document.documentElement){this.addEventListener('touchstart',onTouchStart,false)}});return this}})(jQuery);
+
+$(document).ready(function(){
+	
+	//触摸触发事件
+	$("body").touchwipe({
+		wipeDown: function() {
+			wipe_down();
+		 },
+		 wipeUp: function() { 
+			wipe_up();
+		 },
+		 wipeLeft: function() {
+			wipe_right();
+		 },
+		 wipeRight: function() { 
+		 	wipe_left();
+		 },
+		min_move_x: 80,
+		min_move_y: 80,
+		preventDefaultEvents: true
+	});
+	
+	function wipe_right(){
+		
+	}
+	
+	function wipe_left(){
+		
+	}
+	
+	function wipe_up(){
+		
+	}
+	
+	function wipe_down(){
+		
+	}
+	
+	/*loading*/
+	
+	var img_list={
+		"cover":{
+			"img_1":"bg.jpg"
+		}
+	};
+	
+	//console.log(img_list.cover);
+	var img_array=[];
+	
+	//获得图片的地址
+	for(var i in img_list){
+		if(typeof(img_list[i])=="object"){
+			for(var j in img_list[i]){
+				img_array.push("../mobile/images/"+i+"/"+img_list[i][j]);
+			}
+		}
+	}
+	
+	var img_num=img_array.length;
+	var img_loaded=0;
+	
+	for(var i=0;i<img_num;i++){
+		var img=new Image();
+		img.src=img_array[i];
+		img.onload=function(){
+			
+			img_loaded++;
+			if(img_num==img_loaded){
+				
+				//loadSound(); 如果有声音
+				pageStart(); //载完图片，直接进入首页，音频自己慢慢载，载完自动播放
+			}
+		}
+	}
+	
+	function pageStart(){
+		
+		$(".loading_wrap").addClass("loading_hide");
+		$(".con_wrap_1").addClass("wrap_show");
+		var timer=setTimeout(function(){
+			$(".loading_wrap").hide();
+		},300);
+		no_wipe=0;
+	}
+	
+	function loadSound(){
+		
+	}
+	
+	/*loading*/
+	
+	var ripple_container=$(".con_wrap");
+	
+	
+	function createRipple(){
+		var time_delay=Math.random();
+		var p_x=Math.round(Math.random()*320);
+		var p_y=Math.round(Math.random()*502);
+		var num_scale=Math.random();
+		var ripple_type=Math.round(Math.random());
+		
+		if(num_scale<=0.2){
+			num_scale=0.2;
+		}
+		if(ripple_type){
+			var ripple_template="<div class='ripple_hook' style='left:"+p_x+"px; top:"+p_y+"px; -webkit-transform:scale("+num_scale+");'><div class='ripple_wrap ripple_show' style='-webkit-animation-delay:"+time_delay+"s;'><div class='ripple ripple_1' style='-webkit-animation-delay:"+time_delay+"s;'></div><div class='ripple ripple_2' style='-webkit-animation-delay:"+time_delay+"s;'></div><div class='ripple ripple_3' style='-webkit-animation-delay:"+time_delay+"s;'></div><div class='ripple ripple_4' style='-webkit-animation-delay:"+time_delay+"s;'></div></div></div>";
+		}else{
+			var ripple_template="<div class='ripple_hook' style='left:"+p_x+"px; top:"+p_y+"px; -webkit-transform:scale("+num_scale+");'><div class='ripple_wrap ripple_show' style='-webkit-animation-delay:"+time_delay+"s;'><div class='ripple ripple_1' style='-webkit-animation-delay:"+time_delay+"s;'></div><div class='ripple ripple_2' style='-webkit-animation-delay:"+time_delay+"s;'></div></div></div>";
+		}
+		ripple_container.append(ripple_template);
+	}
+	
+	 var timer=setInterval(function(){
+	 	createRipple();
+	 },200);
+	
+	/* 安卓版本兼容 */
+	var brower = {
+		versions:function(){
+			var u = window.navigator.userAgent;
+			var num ;
+			if(u.indexOf('Trident') > -1){
+			//IE
+				return "IE";
+			}else if(u.indexOf('Presto') > -1){
+			//opera
+				return "Opera";
+			}else if(u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1){
+			//firefox
+				return "Firefox";
+			}else if(u.indexOf('AppleWebKit' && u.indexOf('Safari') > -1) > -1){
+			//苹果、谷歌内核
+				if(u.indexOf('Chrome') > -1){
+				//chrome
+					return "Chrome";
+				}else if(u.indexOf('OPR')){
+				//webkit Opera
+					return "Opera_webkit"
+				}else{
+				//Safari
+					return "Safari";
+				}
+			}else if(u.indexOf('Mobile') > -1){
+			//移动端
+				if(!!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)){
+				//ios
+					if(u.indexOf('iPhone') > -1){
+					//iphone
+						return "iPhone"
+					}else if(u.indexOf('iPod') > -1){
+					//ipod
+						return "iPod"
+					}else if(u.indexOf('iPad') > -1){
+					//ipad
+						return "iPad"
+					}
+				}else if(u.indexOf('Android') > -1 || u.indexOf('Linux') > -1){
+				//android
+					num = u.substr(u.indexOf('Android') + 8, 3);
+					return {"type":"Android", "version": num};
+				}else if(u.indexOf('BB10') > -1 ){
+				//黑莓bb10系统
+					return "BB10";
+				}else if(u.indexOf('IEMobile')){
+				//windows phone
+					return "Windows Phone"
+				}
+			}
+		}
+    }
+	
+	var system=brower.versions();
+	
+	if(system){
+		if(system.type=="Android"){
+			if(system.version==4.4){
+				$("body").addClass("android_version_4_4");
+			}
+			else{
+				$("body").addClass("android_version");
+			}
+		}
+	}
+	
+	
+	
+	/*微信转发图片*/
+	
+	var location_str=location.href;
+	var location_reg="mobile/";
+	//stri.split(reg)[0]+"/images/cover/icon_webank.jpg";
+	//console.log(stri.split(reg));
+	//console.log(location.href);
+	//console.log(location.href.split("mobile/")[0]+"mobile/"+"images/cover/icon_webank.jpg");
+	
+	
+	var imgUrl = location_str.split(location_reg)[0]+location_reg+"images/cover/icon_webank.jpg";
+	var lineLink = location.href;
+	var descContent = "我们是银行？我们是互联网？We是互联网银行。";
+	var shareTitle = document.title;
+	var appid = '';
+	
+	function shareFriend() {
+		WeixinJSBridge.invoke('sendAppMessage',{
+			"appid": appid,
+			"img_url": imgUrl,
+			"img_width": "200",
+			"img_height": "200",
+			"link": lineLink,
+			"desc": descContent,
+			"title": shareTitle
+		}, function(res) {
+			//_report('send_msg', res.err_msg);
+		})
+	}
+	function shareTimeline() {
+		WeixinJSBridge.invoke('shareTimeline',{
+			"img_url": imgUrl,
+			"img_width": "200",
+			"img_height": "200",
+			"link": lineLink,
+			"desc": descContent,
+			"title": shareTitle
+		}, function(res) {
+			   //_report('timeline', res.err_msg);
+		});
+	}
+	function shareWeibo() {
+		WeixinJSBridge.invoke('shareWeibo',{
+			"content": descContent,
+			"url": lineLink,
+		}, function(res) {
+			//_report('weibo', res.err_msg);
+		});
+	}
+	// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+	document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+		// 发送给好友
+		WeixinJSBridge.on('menu:share:appmessage', function(argv){
+			shareFriend();
+		});
+		// 分享到朋友圈
+		WeixinJSBridge.on('menu:share:timeline', function(argv){
+			shareTimeline();
+		});
+		// 分享到微博
+		WeixinJSBridge.on('menu:share:weibo', function(argv){
+			shareWeibo();
+		});
+	}, false);
+	
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
